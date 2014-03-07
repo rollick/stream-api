f43197d9f9906054f384fc735ff64c3334e485a9
Loek van Gent <loek@1procentclub.nl>
2014-03-07 15:37:06 +0100

    Remove api.py rename rest_test to api

--- api.py
+++ api.py
@@ -1,13 +1,53 @@
 import cherrypy
 
+messages = {
+    '1': {
+        'text': 'Pull request #76: Smashing new stylez',
+        'source': 'github'
+    },
 
-class HelloWorld(object):
-    def index(self):
-        return "Hello World!"
-    index.exposed = True
+    '2': {
+        'text': 'Pull request #77: New stuff',
+        'source': 'github'
+    },
 
+    '3': {
+        'text': 'Bart donated 50 bucks',
+        'source': 'donations'
+    }
+}
+
+
+class Message:
+
+    def GET(self, id=None):
+
+        if id == None:
+            result = ('Here are all the messages we have: %s' % messages)
+        elif id in messages:
+            message = messages[id]
+            result = ('Message with the ID %s is called %s, and the artist is %s' % (id, message['text'], message['source']))
+        else:
+            result = ('No message with the ID %s :-(' % id)
+
+        return result
+    
+    def POST(self, message):
+
+        id = str(max([int(_) for _ in messages.keys()]) + 1)
+        messages[id] = message
+        return ('Create a new message with the ID: %s' % id)
+
+    exposed=True
 
 if __name__ == '__main__':
-    root = HelloWorld()
-    cherrypy.quickstart(root)
 
+    cherrypy.tree.mount(
+        Message(), '',
+        {'/':
+            {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}
+        }
+    )
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
